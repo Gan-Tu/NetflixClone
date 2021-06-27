9ca8da4c59eda43ad4bdde40869067a9c336e825
Gan Tu <github@tugan.dev>
2021-06-27 16:38:52 -0700

    Initial Commit

---
+++ Shared/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  Shared
+//
+//  Created by Gan Tu on 6/27/21.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
